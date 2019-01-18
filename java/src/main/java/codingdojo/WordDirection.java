15c2b2330f67d12085b2a98aba7f3c531cfe6c3a
fabien lamarque <fabien.lamarque@gmail.com>
2019-01-18 15:11:57 +0100

    Delete dead code

--- java/src/main/java/codingdojo/WordDirection.java
+++ java/src/main/java/codingdojo/WordDirection.java
@@ -3,27 +3,16 @@ package codingdojo;
 public enum WordDirection {
     HORIZONTAL {
         @Override
-        public Square move(Square previous) {
-            return previous.right();
-        }
-
-        @Override
         public SquareKT move(SquareKT previous) {
             return previous.right();
         }
     },
     VERTICAL {
         @Override
-        public Square move(Square previous) {
-            return previous.down();
-        }
-
-        @Override
         public SquareKT move(SquareKT previous) {
             return previous.down();
         }
     };
 
-    public abstract Square move(Square previous);
     public abstract SquareKT move(SquareKT previous);
 }
